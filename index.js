09768b304347776862e856548518b51586bf331b
Hemendra Zerotime <hemendra.zerotimesolutions@gmail.com>
2024-12-02 16:01:05 +0530

    add the swap ui with functionality

--- index.js
+++ index.js
@@ -20,3 +20,40 @@ let isPrime = () => {
     }
 
 }
+
+let swapNum = () => {
+    let inp1 = parseInt(document.getElementById("inp1").value)
+    let inp2 = parseInt(document.getElementById("inp2").value)
+
+    document.getElementById("In1").innerHTML = `Before Swap First value = ${inp1}`
+    document.getElementById("In2").innerHTML = `Before Swap Second value = ${inp2}`
+
+    if (isNaN(inp1) || isNaN(inp2)) {
+        alert("Type valid number")
+
+    }
+
+    else {
+        console.log("before", inp1)
+        console.log("before", inp2)
+
+
+        inp1 = inp1 + inp2;
+        console.log("inp11", inp1);
+        inp2 = inp1 - inp2
+        console.log("inp12", inp2);
+        inp1 = inp1 - inp2;
+        console.log("inp13", inp1);
+
+        console.log("after", inp1)
+        console.log("after", inp2)
+
+        document.getElementById("out1").innerHTML = `After Swap First value = ${inp1}`
+        document.getElementById("out2").innerHTML = `After Swap Second value = ${inp2}`
+
+
+
+    }
+
+
+}
